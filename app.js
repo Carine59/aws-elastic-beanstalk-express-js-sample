9e8d5578f3e79123cd8c527810b340ba0b4b35fc
Carine59 <62378817+Carine59@users.noreply.github.com>
2022-08-21 08:55:17 +0200

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Lille'));
+app.get('/', (req, res) => res.send('Londres'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
